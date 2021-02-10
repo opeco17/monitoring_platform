15e3858822292457dc9301a15501bcb7529324c4
opeco17 <opeco17@gmail.com>
2021-02-11 01:04:00 +0900

    delete config

--- start.sh
+++ start.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-kubectl apply -f kubernetes-manifests/general-config.yaml
-kubectl apply -f kubernetes-manifests/elasticsearch.yaml
-kubectl apply -f kubernetes-manifests/kibana.yaml
+# Build docker image
+base_path=$(pwd)
+echo $base_path
+for dir in 'metrics-getter' 'anomaly-detector'; do
+    cd src/$dir
+    chmod +x build.sh
+    ./build.sh
+    cd $base_path
+done
 
-# Wait to run Elasticsearch
-sleep 60
+# # Run Kubernetes
+# kubectl apply -f kubernetes-manifests/general-config.yaml
+# kubectl apply -f kubernetes-manifests/elasticsearch.yaml
+# kubectl apply -f kubernetes-manifests/kibana.yaml
 
-kubectl apply -f kubernetes-manifests/metrics-getter.yaml
+# # Wait to run Elasticsearch
+# sleep 60
 
-# Wait to get a sufficient number of metrics
-sleep 180
+# kubectl apply -f kubernetes-manifests/metrics-getter.yaml
 
-kubectl apply -f kubernetes-manifests/anomaly-detector.yaml
+# # Wait to get a sufficient number of metrics
+# sleep 180
+
+# kubectl apply -f kubernetes-manifests/anomaly-detector.yaml
